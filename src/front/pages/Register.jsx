5ca12438314f02ca7963977f56a525c973974a83
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-06-21 23:00:25 +0000

    Add register functionality

--- src/front/pages/Register.jsx
+++ src/front/pages/Register.jsx
@@ -1,59 +1,105 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 
+const initialStateUser = {
+    name: "",
+    lastname: "",
+    email: "",
+    password: ""
+}
+
 export function Register() {
+    const [user, setUser] = useState(initialStateUser)
+    const navigate = useNavigate()
+
+    function handleChange({target}){
+        setUser({
+            ...user,
+            [target.name]: target.value
+        })
+    }
+
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        const url = import.meta.env.VITE_BACKEND_URL;
+        
+        const response = await fetch(`${url}/register`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+
+        if (response.status === 201){
+            setUser(initialStateUser)
+            setTimeout(() => {
+                navigate('/login')
+            }, 1000)
+        } else if (response.status === 400){
+            alert('El usuario ya existe')
+        } else {
+            alert('Error al registrar el usuario')
+        }
+    }
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <h2 className="text-center my-3">Registrate</h2>
                 <div className="col-12 col-md-6">
-                    <form className="border rounded m-2 p-4" >
+                    <form className="border rounded m-2 p-4" onSubmit={handleSubmit} >
                         <div className="form-floating mb-3">
                             <input
                                 type="text"
                                 className="form-control"
-                                id="floatingInput"
+                                id="nameInput"
                                 name="name"
                                 placeholder="name"
+                                onChange={handleChange}
                                 required
                             />
-                            <label for="floatingInput">Name</label>
+                            <label htmlFor="nameInput">Name</label>
                         </div>
 
                         <div className="form-floating mb-3">
                             <input
                                 type="text"
                                 className="form-control"
-                                id="floatingInput"
-                                name="lastName"
+                                id="lastnameInput"
+                                name="lastname"
                                 placeholder="last name"
+                                onChange={handleChange}
                                 required
                             />
-                            <label for="floatingInput">Last name</label>
+                            <label htmlFor="lastnameInput">Last name</label>
                         </div>
 
                         <div className="form-floating mb-3">
                             <input
                                 type="email"
                                 className="form-control"
-                                id="floatingInput"
+                                id="emailInput"
                                 name="email"
                                 placeholder="name@example.com"
+                                onChange={handleChange}
                                 required
                             />
-                            <label for="floatingInput">Email</label>
+                            <label htmlFor="emailInput">Email</label>
                         </div>
 
                         <div className="form-floating mb-3">
                             <input
                                 type="password"
                                 className="form-control"
-                                id="floatingInput"
+                                id="passwordInput"
                                 name="password"
                                 placeholder="password"
+                                onChange={handleChange}
                                 required
                             />
-                            <label for="floatingInput">Contraseña</label>
+                            <label htmlFor="passwordInput">Contraseña</label>
                         </div>
                         <button className="btn btn-outline-primary w-100">Registrarme</button>
                     </form>
