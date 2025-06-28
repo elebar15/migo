a54cc8cc2b5831e4f0aa1245d778b6ce87d879ea
Dani Cornejo <dcornejo.h@gmail.com>
2025-06-28 00:16:09 +0000

    logout

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    const navigate = useNavigate();
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Migo</span>
-				</Link>
-				<div className="ml-auto">
-				</div>
-			</div>
-		</nav>
-	);
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user_id");
+        navigate("");
+    };
+
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container d-flex justify-content-between align-items-center">
+                <Link to="/" className="navbar-brand mb-0 h1">
+                    Migo
+                </Link>
+
+                {localStorage.getItem("token") && (
+                    <button className="btn btn-outline-danger" onClick={handleLogout}>
+                        Cerrar sesión
+                    </button>
+                )}
+            </div>
+        </nav>
+    );
 };
