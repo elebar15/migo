d4380f9012070def7deecc994c523a63ba30d468
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-06-30 16:57:43 +0000

    make delete medical record function in services

--- src/front/services/api.jsx
+++ src/front/services/api.jsx
@@ -19,25 +19,6 @@ async function getAllPets() {
     }
 };
 
-async function getMedicalRecords(pet_id) {
-    const token = localStorage.getItem("token");
-
-    try {
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/pets/${pet_id}/clin-history`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-
-        if (!response.ok) throw new Error('No autorizado');
-        const data = await response.json();
-        return data;
-
-    } catch (error) {
-        console.error("Error al obtener registros medicos:", error)
-        return null;
-    }
-}
 
 async function getPetById(id) {
     const token = localStorage.getItem("token");
@@ -60,8 +41,49 @@ async function getPetById(id) {
     }
 }
 
+async function getMedicalRecords(pet_id) {
+    const token = localStorage.getItem("token");
+
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/pets/${pet_id}/clin-history`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) throw new Error('No autorizado');
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("Error al obtener registros medicos:", error)
+        return null;
+    }
+}
+
+async function deleteMedicalRecord(noteId) {
+  const token = localStorage.getItem("token");
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/clin-history/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    if (!response.ok) throw new Error("Error al eliminar el registro");
+    return true;
+
+  } catch (error) {
+    console.error("Error eliminando registro:", error);
+    return false;
+  }
+}
+
 export {
     getAllPets,
     getPetById,
-    getMedicalRecords
+    getMedicalRecords,
+    deleteMedicalRecord
 }
