8ec89d56dc138574edd1cfc729331828791b2bff
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-07-07 04:19:14 +0000

    border rounded on card

--- src/front/components/PetCard.jsx
+++ src/front/components/PetCard.jsx
@@ -4,17 +4,17 @@ export function PetCard({ pet }) {
     const defaultImage = "https://img.freepik.com/vector-gratis/concepto-mascotas-diferentes_52683-37549.jpg";
 
     return (
-        <div className="card my-card h-100 shadow overflow-hidden" style={{ border: "none" }}>
+        <div className="card my-card h-100 shadow overflow-hidden rounded-4" style={{ border: "none" }}>
             <Link to={`/pet-detail/${pet.id}`} className="text-decoration-none text-dark">
                 <div className="img-container">
                     <img
                         src={pet.image || defaultImage}
                         alt={pet.name || "Mascota"}
-                        className="card-image"
+                        className="card-image "
                     />
                 </div>
                 <div className="card-body text-center">
-                    <h3 className="card-title fw-bold dynapuff">{pet.name}</h3>
+                    <h3 className="card-title dynapuff">{pet.name}</h3>
                 </div>
             </Link>
         </div>
