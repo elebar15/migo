bcc8ce1e3d65f27377e50e60ad18220a91a861d3
elebar15 <dev@higo.pm>
2025-08-27 09:45:17 -0500

    Eliminate credentials in clear and unecessary code

--- src/front/pages/EditPet.jsx
+++ src/front/pages/EditPet.jsx
@@ -21,14 +21,6 @@ export function EditPet() {
         return date.toISOString().split('T')[0]; 
     };
 
-    const formatDisplayDate = (dateStr) => {
-        const date = new Date(dateStr);
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0'); 
-        const year = date.getFullYear();
-        return `${day}-${month}-${year}`; 
-    };
-
     useEffect(() => {
         const token = localStorage.getItem("token");
         if (!token) {
